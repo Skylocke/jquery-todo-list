de26661f0368621b7f3aae86bc59c959b852a687
skylocke <mathematical.fiction@gmail.com>
2016-12-05 21:21:58 -0800

    most basic requirements done

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  console.log("DOM is ready.");
+});
+
+$("#add-text").click(function(){
+  // add input to list
+  var entry = $("#text-input");
+  var del_button = "<button class=\"cross-off\">✓</button>"
+  $("#to-do").append("<div class=\"list-item\">" + del_button + " " + entry.val() + "</div>");
+  $(".cross-off").click(did_the_thing);
+  entry.val("");
+});
+
+function did_the_thing() {
+  $(this).closest('div').remove();
+}
